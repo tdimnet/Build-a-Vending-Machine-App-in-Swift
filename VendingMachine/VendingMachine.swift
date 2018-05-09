02e73ae41988e12ae375a371ba6c6780bab67c47
tdimnet <thomas.dimnet@gmail.com>
2018-05-09 16:58:01 +0200

    Add protocol and enum to discribe the vending machine

---
+++ VendingMachine/VendingMachine.swift
@@ -0,0 +1,72 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Thomas Dimnet on 09/05/2018.
+//  Copyright © 2018 Treehouse Island, Inc. All rights reserved.
+//
+
+import Foundation
+
+enum VendingSelection {
+    case soda
+    case dietSoda
+    case chips
+    case cookie
+    case sandwich
+    case wrap
+    case candyBar
+    case popTart
+    case water
+    case fruitJuice
+    case sportsDrink
+    case gum
+}
+
+protocol VendingItem {
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+
+protocol VendingMachine {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: VendingItem])
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws
+    func deposit(_ amout: Double)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
